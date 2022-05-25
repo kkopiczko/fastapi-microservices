2ada7e8ec249e939d6b1dde5c085c26996da5d83
Kamila <kamkopiczko@gmail.com>
2022-05-25 20:43:46 +0200

    created basic inventory api with temporary fake database

---
+++ inventory/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException, status
+import random
+import schemas
+
+app = FastAPI()
+
+inventory = []
+
+@app.get('/products')
+def get_products():
+    return inventory
+
+@app.get('/products/{product_id}')
+def get_product(product_id: int):
+    product = None
+    for p in inventory:
+        if p['id'] == product_id:
+            product = p
+            break
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'product with id: {product_id} was not found in the inventory')
+
+    return product
+
+@app.post('/products')
+def create_product(product: schemas.Product):
+    product = product.dict()
+    product['id'] = random.randint(1, 10000)
+    print(product)
+    inventory.append(product)
+    return product
+
