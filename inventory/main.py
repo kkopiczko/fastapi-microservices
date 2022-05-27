5d3ad65ddc4b162d395cd2dc0fe5f36645bf0c8e
Kamila <kamkopiczko@gmail.com>
2022-05-27 19:36:23 +0200

    added CORS middleware

--- inventory/main.py
+++ inventory/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 import random
 import schemas
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 inventory = []
 
